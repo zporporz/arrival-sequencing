1052a89fc26df8595dbd6340f1aef3adad457bff
zporporz <ecgkasit@gmail.com>
2026-08-15 23:27:30 +0700

    Enable time workflow guidance and mismatch warnings

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import './polish.css'
 import './production.css'
 import './readability.css'
 import './warning.css'
+import './timeWorkflow.css'
 import { installSpreadsheetNavigation } from './spreadsheetNavigation'
 import { installSpacingGuard } from './spacingGuard'
 import { installFieldGuide } from './fieldGuide'
+import { installTimeWorkflow } from './timeWorkflow'
 import App from './App'
 
 installSpreadsheetNavigation()
 installSpacingGuard()
 installFieldGuide()
+installTimeWorkflow()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
