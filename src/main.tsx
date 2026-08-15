6ed6cf7f9af0ed65fb26eeaa3609bd4ab1dd4f91
zporporz <ecgkasit@gmail.com>
2026-08-15 22:41:12 +0700

    Enable spreadsheet keyboard navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './polish.css'
+import { installSpreadsheetNavigation } from './spreadsheetNavigation'
 import App from './App'
+
+installSpreadsheetNavigation()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
