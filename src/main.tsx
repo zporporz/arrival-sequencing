3c93187bf1d42347015039dc40eff0e8dc65101b
zporporz <ecgkasit@gmail.com>
2026-08-15 23:33:27 +0700

    Enable VTBD flow selector UI

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,19 @@ import './production.css'
 import './readability.css'
 import './warning.css'
 import './timeWorkflow.css'
+import './flowSelector.css'
 import { installSpreadsheetNavigation } from './spreadsheetNavigation'
 import { installSpacingGuard } from './spacingGuard'
 import { installFieldGuide } from './fieldGuide'
 import { installTimeWorkflow } from './timeWorkflow'
+import { installFlowSelector } from './flowSelector'
 import App from './App'
 
 installSpreadsheetNavigation()
 installSpacingGuard()
 installFieldGuide()
 installTimeWorkflow()
+installFlowSelector()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
