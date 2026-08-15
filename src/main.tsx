7a3408bf819d76725c47154eeb70014d5726b213
zporporz <ecgkasit@gmail.com>
2026-08-15 22:58:16 +0700

    Enable production UI and field guide

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './polish.css'
+import './production.css'
 import { installSpreadsheetNavigation } from './spreadsheetNavigation'
 import { installSpacingGuard } from './spacingGuard'
+import { installFieldGuide } from './fieldGuide'
 import App from './App'
 
 installSpreadsheetNavigation()
 installSpacingGuard()
+installFieldGuide()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
