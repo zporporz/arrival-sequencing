9aef4b33cc3f13867556632ecdb2b6743a2d3e8d
zporporz <ecgkasit@gmail.com>
2026-08-15 22:30:42 +0700

    Use consistent 24-hour ATC time inputs

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './polish.css'
+import { installAtcTimeInputs } from './atcTimeInputs'
 import App from './App'
+
+installAtcTimeInputs()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
