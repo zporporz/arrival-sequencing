8b49b3d3d0be40cb43357c5b3f0924199dac066d
zporporz <ecgkasit@gmail.com>
2026-08-15 22:26:44 +0700

    Load sequencing board polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './polish.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
