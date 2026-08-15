fcd52430e1a4e62bfef2753db96dad25e2526a18
zporporz <ecgkasit@gmail.com>
2026-08-15 22:43:07 +0700

    Enable CLDT spacing guard

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './polish.css'
 import { installSpreadsheetNavigation } from './spreadsheetNavigation'
+import { installSpacingGuard } from './spacingGuard'
 import App from './App'
 
 installSpreadsheetNavigation()
+installSpacingGuard()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
