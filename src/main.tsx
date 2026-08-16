fcbdb10e01f1f31b2a9eb930666fc59ef45f0126
zporporz <ecgkasit@gmail.com>
2026-08-16 09:33:35 +0700

    Enable audit identity and cancelled restore

--- src/main.tsx
+++ src/main.tsx
@@ -9,20 +9,25 @@ import './timeWorkflow.css'
 import './flowSelector.css'
 import './workflowControls.css'
 import './lifecyclePanel.css'
+import './restoreCancelled.css'
 import { installSpreadsheetNavigation } from './spreadsheetNavigation'
 import { installSpacingGuard } from './spacingGuard'
 import { installFieldGuide } from './fieldGuide'
 import { installTimeWorkflow } from './timeWorkflow'
 import { installFlowSelector } from './flowSelector'
 import { installLifecyclePanel } from './lifecyclePanel'
+import { installAuditIdentity } from './auditIdentity'
+import { installRestoreCancelled } from './restoreCancelled'
 import App from './App'
 
+installAuditIdentity()
 installSpreadsheetNavigation()
 installSpacingGuard()
 installFieldGuide()
 installTimeWorkflow()
 installFlowSelector()
 installLifecyclePanel()
+installRestoreCancelled()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
