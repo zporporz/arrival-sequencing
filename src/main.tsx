ee599fa20ced6430c31e0105764f80eee52a2fba
zporporz <ecgkasit@gmail.com>
2026-08-17 21:47:10 +0700

    Make AMAN V2 the default live page

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './auth.css'
 import './reactNavbar.css'
 import './adminEditors.css'
 import './sessionAdmin.css'
+import './amanV2.css'
 import { installSpreadsheetNavigation } from './spreadsheetNavigation'
 import { installSpacingGuard } from './spacingGuard'
 import { installFieldGuide } from './fieldGuide'
@@ -22,6 +23,7 @@ import { installLifecyclePanel } from './lifecyclePanel'
 import { installRestoreCancelled } from './restoreCancelled'
 import AuthGate from './AuthGate'
 import AdminPanel from './AdminPanelV2'
+import AmanShell from './AmanShell'
 import App from './App'
 
 installSpreadsheetNavigation()
@@ -32,11 +34,12 @@ installLifecyclePanel()
 installRestoreCancelled()
 
 const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/')
+const useLegacyEditor = new URLSearchParams(window.location.search).get('legacy') === '1'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthGate>
-      {isAdminRoute ? <AdminPanel /> : <App />}
+      {isAdminRoute ? <AdminPanel /> : useLegacyEditor ? <App /> : <AmanShell />}
     </AuthGate>
   </StrictMode>,
 )
