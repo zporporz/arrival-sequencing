408f678d73e241f12784d1f37d0e7c7786c634cf
zporporz <ecgkasit@gmail.com>
2026-08-18 18:10:26 +0700

    Load IVAO Thailand logo from Vite asset at runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './reset.css'
 import './live.css'
-import './logo.css'
 import AuthGate from './AuthGate'
 import App from './App'
+import ivaoThailandLogo from './assets/ivao-thailand-logo.png'
+
+function ArrivalSequencingApp() {
+  useEffect(() => {
+    const logo = document.querySelector<HTMLImageElement>('.aman-brand img')
+    if (logo) logo.src = ivaoThailandLogo
+  }, [])
+
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthGate>
-      <App />
+      <ArrivalSequencingApp />
     </AuthGate>
   </StrictMode>,
 )
