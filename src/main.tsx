f4b56de1bb2e7f9e696393551d2103530f05762d
zporporz <ecgkasit@gmail.com>
2026-08-18 19:00:28 +0700

    Apply dynamic below-ACTUAL timeline window

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './reset.css'
 import './live.css'
 import './drag.css'
+import './timelineWindow.css'
 import AuthGate from './AuthGate'
 import App from './App'
 
