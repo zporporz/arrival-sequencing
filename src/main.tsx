7ff4dae9a60be5e88781f2df9d27ccbb3d07739f
zporporz <ecgkasit@gmail.com>
2026-08-18 20:03:44 +0700

    Enable automatic MAESTRO flight status colours

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,29 @@ import './live.css'
 import './drag.css'
 import './timelineWindow.css'
 import './timelineAxisRuntime.css'
+import './flightStatus.css'
 import AuthGate from './AuthGate'
 import App from './App'
 import { installTimelineAxisRuntime } from './timelineAxisRuntime'
+import { installFlightStatusRuntime } from './flightStatusRuntime'
 
-function AppWithTimelineRuntime() {
-  useEffect(() => installTimelineAxisRuntime(), [])
+function AppWithRuntime() {
+  useEffect(() => {
+    const removeTimelineRuntime = installTimelineAxisRuntime()
+    const removeFlightStatusRuntime = installFlightStatusRuntime()
+    return () => {
+      removeTimelineRuntime()
+      removeFlightStatusRuntime()
+    }
+  }, [])
+
   return <App />
 }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthGate>
-      <AppWithTimelineRuntime />
+      <AppWithRuntime />
     </AuthGate>
   </StrictMode>,
 )
